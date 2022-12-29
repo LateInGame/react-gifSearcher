ede6322ff101882cdf25494fdf00beb8c1cb701f
David Deus <davidafonsodeus@outlook.com>
2022-12-29 19:48:59 +0000

    gif-list.jsx refractored

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -4,10 +4,19 @@ import React, { Component } from 'react';
 import Gif from './gif';
 
 class GifList extends Component {
+  renderList = () => {
+    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
+  }
+
   render () {
-    return this.props.gifs.map((gif) => {
-      return <Gif id={gif.id} />;
-    });
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
   }
 }
 export default GifList;
+
+// this.props.gifs.map((gif) => {
+//   return <Gif id={gif.id} key={gif.id} />;
