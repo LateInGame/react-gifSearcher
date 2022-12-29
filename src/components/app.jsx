e9b98715b4249e39ecf55a959293b63e84e0dae1
David Deus <davidafonsodeus@outlook.com>
2022-12-29 18:28:44 +0000

    search_bar and gif .jsx added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/self-closing-comp */
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import Gif from './gif';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render () {
     return (
       <div>
-        <div className="left-scene"></div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id="wuBfF1AvGhTHyR1879" />
+          </div>
+        </div>
         <div className="right-scene"></div>
       </div>
     );
---
+++ src/components/gif.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render () {
+    // eslint-disable-next-line react/destructuring-assignment
+    const src = `https://media4.giphy.com/media/${this.props.id}/giphy.gif`;
+    return (
+      <img src={src} alt="" className="gif" />
+    );
+  }
+}
+
+export default Gif;
