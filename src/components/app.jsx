e052390a59f6414fd6892804f20bad4e10d14b2c
David Deus <davidafonsodeus@outlook.com>
2022-12-29 20:43:11 +0000

    connecting giphy api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,27 +1,43 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/self-closing-comp */
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
-
+import giphy from 'giphy-api'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "wuBfF1AvGhTHyR1879"
+    };
+  }
+
+  search = (query) => {
+    giphy('WPQ9aBGtsihYFC9Pee06ndayO8CTXjLz').search({
+      q: query,
+      rating: 'g'
+    }, function (error, result) => {
+      console.log(result),
+    });
+  };
+
   render () {
-    const gifs = [
-      { id: "wuBfF1AvGhTHyR1879" },
-      { id: "KeEqnLBNI4TmvmhUE5" }
-    ];
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="wuBfF1AvGhTHyR1879" />
+            <Gif id={this.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
