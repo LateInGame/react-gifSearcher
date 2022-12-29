ac920b0b6719058359ba0bc98af27d4445d58d27
David Deus <davidafonsodeus@outlook.com>
2022-12-29 17:22:49 +0000

    changed version to 8.0 in loader.js to compile

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/self-closing-comp */
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <div className="left-scene"></div>
+        <div className="right-scene"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
