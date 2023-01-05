e9ba5c4d1ed067f06348ff0b4252d8d0ff95f428
David Deus <davidafonsodeus@outlook.com>
2023-01-05 13:03:58 +0000

    added constructor to search_bar.jsx

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,17 +1,29 @@
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    };
+  }
+
   handleUpdate = (event) => {
     // eslint-disable-next-line react/destructuring-assignment
+    this.setState({
+      term: event.target.value
+    });
     this.props.searchFunction(event.target.value);
-    console.log(event.target.value);
   }
 
   render () {
     return (
       <input
+        value={this.state.term}
         type="text"
         className="form-control form-search"
         onChange={this.handleUpdate}
