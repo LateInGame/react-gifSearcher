88bfd6419d7f4cadf01bbf0fcc0bd79ae3cab0f9
David Deus <davidafonsodeus@outlook.com>
2023-01-05 13:32:34 +0000

    changed gif_list.jsx method

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,21 +1,24 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import Gif from './gif';
 
-class GifList extends Component {
-  renderList = () => {
-    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
-  }
+const GifList = ({ gifs, selectGif }) => {
+  return (
+    <div className="gif-list">
+      {gifs.map(({ id }) => <Gif id={id} key={id} selectGif={selectGif} />)}
+    </div>
+  );
+};
 
-  render () {
-    return (
-      <div className="gif-list">
-        {this.renderList()}
-      </div>
-    );
-  }
-}
+//   render () {
+//     return (
+//       <div className="gif-list">
+//         {this.renderList()}
+//       </div>
+//     );
+//   }
+// }
 export default GifList;
 
 // this.props.gifs.map((gif) => {
